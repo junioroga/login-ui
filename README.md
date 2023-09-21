244bc215a1bc40605b51e86c3c2ad331859026c0
junioroga <junioroga@outlook.com>
2023-09-21 16:05:19 -0300

    react native with tailwind css, responsive width and height, and animated with layout animations

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# login-ui
-Login and Signup UI with Tailwind css and Reanimated
+# React Native Login UI App
+
+This is a React Native App to show my first contact with Tailwind CSS + React Native components, and made some animations
+using Layout Animation by Reanimated.
+
+Project created with Expo CLI. [See official docs](https://docs.expo.dev/get-started/installation/) to set up the environment.
+
+## 🛠 Technologies Used 
+
+The following tools were used to build the project:
+
+- [Expo](https://expo.io/)
+- [React Native](https://reactnative.dev/)
+- [React Navigation](https://reactnavigation.org/docs/getting-started/): Routes and navigation
+- [React Native Reanimated](https://docs.swmansion.com/react-native-reanimated/): React Native Reanimated is a powerful animation library.
+- [Native Wind](https://www.nativewind.dev/): NativeWind uses Tailwind CSS as scripting language to create a universal style system for React Native. NativeWind components can be shared between platforms and will output their styles as CSS StyleSheet on web and StyleSheet.create for native.
+
+## 🚀 Open and run the project
+
+First you need to install the native packages and dependencies.
+
+```
+yarn
+```
+
+Run on IOS
+
+```
+yarn ios
+```
+
+Run on Android
+
+```
+yarn android
+```
+
+## Demonstration
+
+### IOS
+<img src="/assets/readme/ios.gif" width="50%" height="50%"/>
+
+### Android
+
+<img src="/assets/readme/android.gif" width="50%" height="50%"/>
+
+### Smaller Device
+
+<img src="/assets/readme/android.gif" width="50%" height="50%"/>
---
+++ screens/SignupScreen.js
@@ -0,0 +1,57 @@
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import Animated, { FadeInDown, FadeInUp } from 'react-native-reanimated'
+import { useNavigation } from '@react-navigation/native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+export default function SignupScreen() {
+  const navigation = useNavigation()
+
+  return (
+    <View className="bg-white h-full w-full">
+      <StatusBar style="light" />
+      <Image className="absolute" style={{ height: hp(90), width: wp(100)}} source={require('../assets/images/background.png')} />
+
+      {/* lights */}
+      <View className="flex-row justify-around w-full absolute">
+        <Animated.Image resizeMode="contain" style={{ height: hp(23), width: wp(23)}} entering={FadeInUp.delay(200).duration(1000).springify()} source={require('../assets/images/light.png')} />
+        <Animated.Image resizeMode="contain" style={{ height: hp(17), width: wp(17)}} entering={FadeInUp.delay(400).duration(1000).springify()} source={require('../assets/images/light.png')} />
+      </View>
+
+      {/* title and form */}
+      <View className="h-full w-full flex justify-around">
+        {/* title */}
+        <View style={{position: 'absolute', top: hp(25), alignSelf: 'center'}} className="flex items-center">
+          <Animated.Text style={{ fontSize: hp(6) }} entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wider">
+            Sign Up
+          </Animated.Text>
+        </View>
+
+        {/* form */}
+        <View style={{ top: hp(20) }} className="flex items-center mx-4 space-y-4">
+          <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder='Username' placeholderTextColor={'gray'}/>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full">
+            <TextInput placeholder='Email' placeholderTextColor={'gray'}/>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="bg-black/5 p-5 rounded-2xl w-full mb-3">
+            <TextInput placeholder='Password' placeholderTextColor={'gray'} secureTextEntry/>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(600).duration(1000).springify()} className="w-full">
+            <TouchableOpacity className="w-full bg-sky-400 p-3 rounded-2xl">
+              <Text style={{ fontSize: hp(3) }}className="font-bold text-white text-center">SignUp</Text>
+            </TouchableOpacity>
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(800).duration(1000).springify()} className="flex-row justify-center">
+            <Text>Already have an account? </Text>
+            <TouchableOpacity onPress={() => navigation.push('Login')}>
+              <Text className="text-sky-600">Login</Text>
+            </TouchableOpacity>
+          </Animated.View>
+        </View>
+      </View>
+    </View>
+  )
+}
